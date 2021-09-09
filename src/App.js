0fa3144b3cf69ebdf9383ab7c27160fb141455b9
Chris Dunne <chrisdunne66@gmail.com>
2021-09-09 18:47:41 +0100

    0.1 - Getting Setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+// import backgroundImage from '../../assets/backgroundImage.jpeg'
+import test from './assets/background_image.jpeg'
+import React from 'react';
 
 function App() {
+const imgUrl = "./assets/background_image.jpeg";
+
+console.log("Render")
+
+  const style = {
+    backgroundImage: 'url(' + imgUrl + ')',
+    backgroundPosition: "bottom",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    height: "auto",
+    // maxWidth:"1200px",
+    width:"100%",
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={style}>
+     <div className="wrapper" style={style}>
+       <div className="header" style={style}>
+         <h1>Section 1</h1>
+      </div>
+       <div><h1>Section 2</h1></div>
+       <div><h1>Section 3</h1></div>
+       <div><h1>Section 4</h1></div>
+
+       <div><h1>Section 5</h1></div>
+       <div><h1>Section 6</h1></div>
+       <div><h1>Section 7</h1></div>
+       <div><h1>Section 8</h1></div>
+     </div>
     </div>
   );
 }
