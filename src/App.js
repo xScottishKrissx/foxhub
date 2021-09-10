eb1dd23b5e05ec07456da3036f7269fdf75dba8f
Chris Dunne <chrisdunne66@gmail.com>
2021-09-10 11:32:57 +0100

    0.3 - Section 2 - Image row in
    Not sure about the box shadow but it will do for now

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ import './App.css';
 import backgroundImage from './images/background_image.jpeg'
 import coleSkateboard from './images/cole_skateboard.png'
 import skateType from './images/skate_type.png'
-
+import deck from './images/deck_1.jpg';
+import hoodie from './images/hoodie_1.jpg';
+import wheels from './images/wheels_1.jpg';
+import shoes from './images/shoes_1.jpg';
 function App() {
 
 
@@ -34,48 +37,50 @@ console.log("Render")
           <img id="skateType" alt="text" src={skateType} />
         </span>
 
-
          <h1>THE HOME OF SKATEBOARDING</h1>
          <p>Things change, boards change, shoes changes, tricks change, skaters change</p>
-         
          
          <button>LEARN MORE</button>
 
          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#fff" fill-opacity="1" d="M0,96L60,80C120,64,240,32,360,42.7C480,53,600,107,720,160C840,213,960,267,1080,272C1200,277,1320,235,1380,213.3L1440,192L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
 
-
          {/* M78,200 c175,-72 166,76 348,2 */}
 
-
-
          <span className="gradientSection"></span>
+
+
        </div>
 
-        <div>
-            <p>Keep it fresh</p>
-           <h2>BRAND NEW PRODUCTS</h2>
-           <div className="imageRow">
+  <div className="section2">
+    <p>Keep it fresh</p>
+    <h2>BRAND NEW PRODUCTS</h2>
+      <div className="imageRow">
 
-             <div className="imageRowItem">
-               <span>RIPNDIP BUTTERFLY DECK LAVENDER - 8.25"</span>
-             </div>
-
-             <div className="imageRowItem">
-               <span>THRASHER MAG PULLOVER HOOD - BLACK</span>
-             </div>
-
-             <div className="imageRowItem">
-               <span>SPITFIRE WHEELS NATURAL/YELLOW - 55MM</span>
-             </div>
-
-             <div className="imageRowItem">
-               <span>DC SWITCH SKATE SHOE -WHITE/GUM</span>
-             </div>
-
-           </div>
-           <button>SEE MORE</button>
-           
+        <div className="imageRowItem">
+          <img src={deck} alt="text" />
+          <span>RIPNDIP BUTTERFLY DECK LAVENDER - 8.25"</span>
         </div>
+
+        <div className="imageRowItem">
+          <img src={hoodie} alt="text" />
+          <span>THRASHER MAG PULLOVER HOOD - BLACK</span>
+        </div>
+
+        <div className="imageRowItem">
+          <img src={wheels} alt="text" />
+          <span>SPITFIRE WHEELS NATURAL/YELLOW - 55MM</span>
+        </div>
+
+        <div className="imageRowItem">
+          <img src={shoes} alt="text" />
+          <span>DC SWITCH SKATE SHOE -WHITE/GUM</span>
+        </div>
+
+      </div>
+
+    <button>SEE MORE</button>
+
+  </div>
 
 
 
