d143789697434cb1e670709a144b0532cc671c1a
Chris Dunne <chrisdunne66@gmail.com>
2021-09-13 12:21:03 +0100

    0.7 - First stage Complete
    I have the look of the design in place as best I can. I can't figure out the wavy lines or the double outline border buttons, so instead of banging my head against the wall I am focusing on things I can do. If i have time later i will come back and try again

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import shoes from './images/shoes_1.jpg';
 import interviewImage from './images/interview_image.jpg'
 import socialImage  from './images/social_image.jpeg'
 import stars from './images/stars_1.png'
+import coleTypeReverse from './images/cole_type_reverse.png'
 
 function App() {
 
@@ -36,25 +37,27 @@ console.log("Render")
 
       <div  className="section1" style={style}>
         
-        
-        <span id="section1ImageWrapper">
-          <img id="coleSkateboard" src={coleSkateboard} alt="text" />
-          <img id="skateType" alt="text" src={skateType} />
-        </span>
+        <header className="headerWrapper">
+          <span id="section1ImageWrapper">
+            <img id="coleSkateboard" src={coleSkateboard} alt="text" />
+            <img id="skateType" alt="text" src={skateType} />
+          </span>
 
-         <h1>THE HOME OF SKATEBOARDING</h1>
-         <p>Things change, boards change, shoes changes, tricks change, skaters change</p>
-         
-         <button>LEARN MORE</button>
+          <h1>THE HOME OF SKATEBOARDING</h1>
+          <p>Things change, Boards change, shoes changes, tricks change, skaters change</p>
+          
+          <button>LEARN MORE</button>
+        </header>
 
-         <svg height="100%" width="100%" id="svg" viewBox="0 0 1440 400" xmlns="http://www.w3.org/2000/svg" class="transition duration-300 ease-in-out delay-150"><path d="M 0,400 C 0,400 0,200 0,200 C 160.8,176 321.6,152 490,167 C 658.4,182 834.4000000000001,236 994,248 C 1153.6,260 1296.8,230 1440,200 C 1440,200 1440,400 1440,400 Z" stroke="none" stroke-width="0" fill="#fff" ></path></svg>
+
+         {/* <svg height="100%" width="100%" id="svg" viewBox="0 0 1440 400" xmlns="http://www.w3.org/2000/svg" class="transition duration-300 ease-in-out delay-150"><path d="M 0,400 C 0,400 0,200 0,200 C 160.8,176 321.6,152 490,167 C 658.4,182 834.4000000000001,236 994,248 C 1153.6,260 1296.8,230 1440,200 C 1440,200 1440,400 1440,400 Z" stroke="none" stroke-width="0" fill="#fff" ></path></svg> */}
          {/* M78,200 c175,-72 166,76 348,2 */}
-
+         {/* <svg height="100%" width="100%" id="svg" viewBox="0 0 1440 400" xmlns="http://www.w3.org/2000/svg" class="transition duration-300 ease-in-out delay-150"><path d="M 0,400 C 0,400 0,200 0,200 C 160.8,176 321.6,152 490,167 C 658.4,182 834.4000000000001,236 994,248 C 1153.6,260 1296.8,230 1440,200 C 1440,200 1440,400 1440,400 Z" stroke="none" stroke-width="0" fill="white" ></path></svg> */}
          <span className="gradientSection"></span>
 
 
        </div>
-
+{/* <svg height="100%" width="100%" id="svg" viewBox="0 0 1440 400" xmlns="http://www.w3.org/2000/svg" class="transition duration-300 ease-in-out delay-150"><path d="M 0,400 C 0,400 0,200 0,200 C 160.8,176 321.6,152 490,167 C 658.4,182 834.4000000000001,236 994,248 C 1153.6,260 1296.8,230 1440,200 C 1440,200 1440,400 1440,400 Z" stroke="none" stroke-width="0" fill="#fff" ></path></svg> */}
   <div className="section2">
     <p>Keep it fresh</p>
     <h2>BRAND NEW PRODUCTS</h2>
@@ -93,47 +96,48 @@ console.log("Render")
 
        <div className="section3">
 
-         <div className="curved upper">
-
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#EBEBEB" fill-opacity="1" d="M0,128L80,117.3C160,107,320,85,480,106.7C640,128,800,192,960,208C1120,224,1280,192,1360,176L1440,160L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
-
- 
-         </div>
-
-         <div className="imageAndTextRow">
-           <div className="imageAndTextRowText">
-             <h2>INTERVIEW WITH DAEWON SONG</h2>
-             <p> We got to sit down with Daewon at the London premiere for a chat on “what else is out there”</p>
-             <button>READ MORE</button>
-           </div>
-           <div className="imageAndTextRowImage">
-             <span id="interviewImage" className="imageContainer">
-             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 319"><path fill="#EBEBEB" fill-opacity="1" d="M0,1200L1440,111L1440,320L0,320Z"></path></svg>
-             </span>
-             {/* <img src={interviewImage} alt="text" /> */}
+         {/* <div className="curved upper">
+            <svg xmlns="http://www.w3.org/2000/svg" width="100%" viewBox="0 0 1440 320"><path fill="#EBEBEB" fill-opacity="1" d="M0,128L80,117.3C160,107,320,85,480,106.7C640,128,800,192,960,208C1120,224,1280,192,1360,176L1440,160L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg> 
+         </div> */}
+        <div className="imageAndTextRowWrapper">
+          <div className="imageAndTextRow upper">
+            <div className="imageAndTextRowText">
+              <h2>INTERVIEW WITH DAEWON SONG</h2>
+              <p> We got to sit down with Daewon at the London premiere for a chat on “what else is out there”</p>
+              <button>READ MORE</button>
             </div>
-         </div>
-
-         <div className="imageAndTextRow lower">
-           <div className="imageAndTextRowText">
-             <h2>KEEP IT SOCIAL </h2>
-             <p>Follow us on Instagram for the latest tricks from the best skaters around the globe!</p>
-             <button>FOLLOW US</button>
-           </div>
-            <div className="imageAndTextRowImage ">
-              <span id="socialImage" className="imageContainer">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 319"><path fill="#fff" fill-opacity="1" d="M0,1200L1440,111L1440,320L0,320Z"></path></svg>
+            <div className="imageAndTextRowImage">
+              <span id="interviewImage" className="imageContainer">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 319"><path fill="#EBEBEB" fill-opacity="1" d="M0,1200L1440,111L1440,320L0,320Z"></path></svg>
               </span>
-              
-             {/* <img src={socialImage} alt="text" /> */}
+              {/* <img src={interviewImage} alt="text" /> */}
+              </div>
+          </div>
+
+          <div className="imageAndTextRow lower">
+            <div className="imageAndTextRowText">
+              <h2>KEEP IT SOCIAL </h2>
+              <p>Follow us on Instagram for the latest tricks from the best skaters around the globe!</p>
+              <button>FOLLOW US</button>
             </div>
-         </div>
+              <div className="imageAndTextRowImage ">
+                <span id="socialImage" className="imageContainer">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 319"><path fill="#EBEBEB" fill-opacity="1" d="M0,1200L1440,111L1440,320L0,320Z"></path></svg>
+                </span>
+                
+              {/* <img src={socialImage} alt="text" /> */}
+              </div>
+          </div>
+        </div>
+        
+
       
-         <div className="curved lower">
+         {/* <div className="curved lower">
 
 
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#fff" fill-opacity="1" d="M0,32L80,32C160,32,320,32,480,58.7C640,85,800,139,960,133.3C1120,128,1280,64,1360,32L1440,0L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
-         </div>
+
+         </div> */}
 
        </div>
 
@@ -149,7 +153,7 @@ console.log("Render")
 
        <div className="section5">
          <div className="blockWrapper">
-          <div className="singleBlock"></div>
+          {/* <div className="singleBlock"></div> */}
             <div className="block">
               Block 1
               <img src={coleSkateboard} />
@@ -158,9 +162,9 @@ console.log("Render")
             <div className="block">
               <div className="blockContent">
                 <h6>NEWSLETTER</h6>
-                <h2>STAY IN TOUCH</h2>
+                <h2>STAY IN TOUCH !</h2>
                 <p>Get the latest deals, news and gear from Cole Skate Co!</p>
-                <span>Subscribe</span>
+                <button>Subscribe</button>
               </div>
             </div>
          </div>
@@ -170,20 +174,25 @@ console.log("Render")
        </div>
 
        <div className="section6">
-           <div className="footerBottomSectionLeft">
-             <span>DELIVERY</span>
-             <span>TERMS</span>
-             <span>CONTACT US</span>
-           </div>
-           <div className="footerBottomSectionMiddle">
-             <img src={skateType} alt=""/>
-           </div>
-           <div className="footerBottomSectionRight">
-           <span>0141 370 321</span>
-           <span>EMAIL</span>
-           <span>INSTA</span>
-           </div>
+         <div className="footerWrapper">
+
+            <div className="footerBottomSectionLeft">
+              <span>DELIVERY</span>
+              <span>TERMS</span>
+              <span>CONTACT US</span>
+            </div>
+
+            <div className="footerBottomSectionMiddle">
+              <img src={coleTypeReverse} alt=""/>
+            </div>
+            
+            <div className="footerBottomSectionRight">
+              <span>0141 370 321</span>
+              <span>EMAIL</span>
+              <span>INSTA</span>
+            </div>
          </div>
+        </div>
 
 
      </div>
